af49c8c01fa837864ca3e6887bdeb6a55cb807f2
keerthi <palireddy.keerthi@gmail.com>
2020-03-03 22:01:55 +0530

    added sweets object

--- Maven_OOP/src/main/java/com/epam/Maven_OOP/App.java
+++ Maven_OOP/src/main/java/com/epam/Maven_OOP/App.java
@@ -8,5 +8,6 @@ public class App
         Chocolate chocolate = new Chocolate(100, 2, "DairyMilk");
         System.out.println(chocolate.calculateWeight());
         Sweets sweets = new Sweets(100, 3, "Laddu");
+        System.out.println(sweets.calculateWeight());
     }
 }
