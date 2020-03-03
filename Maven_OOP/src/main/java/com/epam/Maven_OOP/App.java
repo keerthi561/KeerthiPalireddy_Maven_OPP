6645aebfcbdab0c15b09c500b66218fb9ccabfb9
keerthi <palireddy.keerthi@gmail.com>
2020-03-03 20:28:27 +0530

    added main class

--- Maven_OOP/src/main/java/com/epam/Maven_OOP/App.java
+++ Maven_OOP/src/main/java/com/epam/Maven_OOP/App.java
@@ -5,6 +5,8 @@ public class App
     public static void main( String[] args )
     {
     	
-//        Chocolate chocolate = new Chocolate("DairyMilk", );
+        Chocolate chocolate = new Chocolate(100, 2, "DairyMilk");
+        System.out.println(chocolate.calculateWeight());
+        Sweets sweets = new Sweets(100, 3, "Laddu");
     }
 }
